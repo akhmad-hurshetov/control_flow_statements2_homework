93f5642ca665cee4b6839d00c85c805f84ed8836
Akhmad <akhmadhurshetov@gmail.com>
2022-10-31 11:40:52 +0500

    05 check

--- if05.py
+++ if05.py
@@ -16,4 +16,16 @@ def main(n):
     n=n//10
     x5=n%10
     n=n//10
-    return
+
+    if x1>x2 and x1>x3 and x1>x4 and x1>x5:
+        return x1
+    if x2>x1 and x2>x3 and x2>x4 and x2>x5:
+        return x2
+    if x3>x1 and x3>x2 and x3>x4 and x3>x5:
+        return x3
+    if x4>x1 and x4>x2 and x4>x3 and x4>x5:
+        return x4
+    if x5>x1 and x5>x2 and x5>x3 and x5>x4:
+        return x5
+        
+    return x1, x2, x3, x4, x5
